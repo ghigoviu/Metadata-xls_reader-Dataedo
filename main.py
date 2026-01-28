38261388f43a226a3b5ac9ee25e46f6fce092d84
Ghigoviu <ghigoviu@gmail.com>
2026-01-28 12:09:15 -0600

    ft Adapatado a catalogo de varias BD

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def append_tables(act_list, new_table):
         act_list.append(table_row)
 
 def get_titles(num_worksheet):
-    row_ini = 11
+    title_row = 11
+    row_ini = title_row
     column_ini = 1
     ws = wb.worksheets[num_worksheet]
     # Cuántas columnas tiene la tabla?
-    row_act = row_ini
+
     column_act = column_ini
     cl = ws.cell(row=row_ini, column=column_ini)
     while cl.value is not None:
@@ -20,7 +21,7 @@ def get_titles(num_worksheet):
 
     titles = ["Type", "Name"]
     num_columns = column_act
-    for row in wb.worksheets[num_worksheet].iter_rows(min_row=10, max_col=num_columns - 1, max_row=10):
+    for row in wb.worksheets[num_worksheet].iter_rows(min_row=title_row, max_col=num_columns - 1, max_row=title_row):
         for cell in row:
             titles.append(cell.value)
     return titles
@@ -28,14 +29,14 @@ def get_titles(num_worksheet):
 if __name__ == '__main__':
     wb = load_workbook('resources/Dataedo Data Dictionary.xlsx')
 
-    tables_list = [get_titles(4)]
+    tables_list = [get_titles(10)]
     functions_list = []
     procedures_list = []
     views_list = []
     all_list = []
     for sheet in wb.worksheets[4:]:
         ws = sheet
-        row_ini = 10
+        row_ini = 11
         column_ini = 1
         # Cuántas columnas tiene la tabla?
         row_act = row_ini
@@ -56,13 +57,14 @@ if __name__ == '__main__':
         num_columns = column_act
         new_list = []
         new_row = ["Type", "Name"]
-        for row in ws.iter_rows(min_row=11, max_col=num_columns-1, max_row=10):
+        titles_row = 11
+        for row in ws.iter_rows(min_row=titles_row, max_col=num_columns-1, max_row=titles_row):
             for cell in row:
                 new_row.append(cell.value)
         new_list.append(new_row)
 
-        for row in ws.iter_rows(min_row=12, max_col=num_columns-1, max_row=num_filas-1):
-            new_row = [ws.cell(row=1, column=1).value, ws.cell(row=4, column=2).value]
+        for row in ws.iter_rows(min_row=titles_row + 1, max_col=num_columns-1, max_row=num_filas-1):
+            new_row = [ws.cell(row=1, column=1).value, ws.cell(row=5, column=2).value]
             for cell in row:
                 new_row.append(cell.value)
             new_list.append(new_row)
@@ -78,10 +80,10 @@ if __name__ == '__main__':
         for e in new_list:
             all_list.append(e)
 
-    HandleCSV.write_csv("./resources/all_list.csv", all_list)
-    HandleCSV.write_csv("./resources/table_list.csv", tables_list)
-    HandleCSV.write_csv("./resources/views_list.csv", views_list)
-    HandleCSV.write_csv("./resources/functions_list.csv", functions_list)
-    HandleCSV.write_csv("./resources/procedures_list.csv", procedures_list)
+    HandleCSV.write_csv("./resources/all_list_full.csv", all_list)
+    HandleCSV.write_csv("./resources/table_list_full.csv", tables_list)
+    HandleCSV.write_csv("./resources/views_list_full.csv", views_list)
+    HandleCSV.write_csv("./resources/functions_list_full.csv", functions_list)
+    HandleCSV.write_csv("./resources/procedures_list_full.csv", procedures_list)
 
 
